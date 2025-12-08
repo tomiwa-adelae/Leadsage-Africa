06a8055750b8cb7f2b9784e4ba6eae642a6d1c66
Tomiwa Adelae <adelaetomiwa6@gmail.com>
2025-12-08 12:58:16 +0100

    WOrked on te categorues

--- app/(root)/_components/Categories.tsx
+++ app/(root)/_components/Categories.tsx
@@ -6,7 +6,7 @@ export const Categories = async () => {
   const categories = await getCategories();
   return (
     <div className="container pt-16 pb-10">
-      <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2">
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2">
         {categories.map((category) => (
           <CategoryCard
             key={category.id}
